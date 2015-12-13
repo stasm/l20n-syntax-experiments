eb342b1a2574d424e6ce2066f60f93f08a75e887
Staś Małolepszy <stas@mozilla.com>
2015-12-13 12:45:32 +0100

    {} syntax for hashes (variant mappings)

--- minimal.clj
+++ minimal.clj
@@ -8,11 +8,15 @@
 ;    - no globals
 ;    - no 'computed' expression syntax (use a built-in macro)
 ;    - no 'this' expression
+;    - no imports
+;    - no local entities
+;    - no implicit default variants
 ; 
 ; 1. translations should be hard to break
 ;    - good error recovery
 ;    - args have $ sigils
 ;    - variant names have : sigils
+;    - variants are easily recognizable by being enclosed with {}
 ;    - only text between quotes should be localized (important for macros' 
 ;      return values; see at the end)
 ;
@@ -24,17 +28,16 @@
 ; keep simple things simple
 <brand-name "Firefox">
 
-<brand-name
-  ; in absence of index the first variant for each nesting becomes the default
-  :nominative "Firefox"
-  :accusative "Firefoksie">
+<brand-name [:nominative]
+  {:nominative "Firefox"}
+  {:accusative "Firefoksie"}>
 
-<brand-name
+<brand-name [:nominative]
   ; the first contiguous comment in the entity's body becomes the docstring
   ; @desc The name of the application
   ^gender "masculine"
-  :nominative "Firefox"
-  :accusative "Firefoksie">
+  {:nominative "Firefox"}
+  {:accusative "Firefoksie"}>
 
 ; no attributes, just conventions not enforced by syntax and used by bindings
 ; triple-quotes for multiline strings
@@ -49,27 +52,28 @@
 
 ; trait access with ^
 <crashed [brand-name^gender]
-  :masculine "{{ brand-name }} uległ awarii"
-  :feminine "{{ brand-name }} uległa awarii">
+  {:masculine "{{ brand-name }} uległ awarii"}
+  {:feminine "{{ brand-name }} uległa awarii"}>
 
-; nested variants are all spelled-out; the entity is a flat mapping of variant 
-; names permutations to variant translations
 ; built-in macros are always namespaced: cldr/*, intl/*, os/* etc.
 <you-received [(cldr/plural $new-messages) $gender]
   ; @param $new-messages Number of new messages
   ; @param $gender The gender of the recepient
-  :one:masculine "Otrzymałeś 1 nową wiadomość."
-  :one:feminine "Otrzymałaś 1 nową wiadomość."
-  :few:masculine "Otrzymałeś {{ $new-messages }} nowe wiadomości."
-  :few:feminine "Otrzymałaś {{ $new-messages }} nowe wiadomości."
-  :many:masculine "Otrzymałeś {{ $new-messages }} nowych wiadomości."
-  :many:feminine "Otrzymałaś {{ $new-messages }} nowych wiadomości.">
+  {:one
+    {:masculine "Otrzymałeś 1 nową wiadomość."}
+    {:feminine "Otrzymałaś 1 nową wiadomość."}}
+  {:few
+    {:masculine "Otrzymałeś {{ $new-messages }} nowe wiadomości."}
+    {:feminine "Otrzymałaś {{ $new-messages }} nowe wiadomości."}}
+  {:many
+    {:masculine "Otrzymałeś {{ $new-messages }} nowych wiadomości."}
+    {:feminine "Otrzymałaś {{ $new-messages }} nowych wiadomości."}}>
 
 ; _-./ are allowed in identifiers
 ; namespaces are also convention
 <songs-list/rating-star.aria-label [(cldr/plural $n)]
-  :one "{{ $n }} star"
-  :many "{{ $n }} stars">
+  {:one "{{ $n }} star"}
+  {:many "{{ $n }} stars"}>
 
 ; in English
 <space-available
@@ -82,13 +86,15 @@
   ; @param $num Number of units available
   ; @param $unit Name of the unit
   [(cldr/plural $num)]
-  :one "{{ $num }}{{ (fr/byte-unit $unit) }} disponible"
-  :many "{{ $num }}{{ (fr/byte-unit $unit) }} disponibles">
+  {:one "{{ $num }}{{ (fr/byte-unit $unit) }} disponible"}
+  {:many "{{ $num }}{{ (fr/byte-unit $unit) }} disponibles"}>
 
 ; custom macros are defined similar to LISP functions
-; the following symbols are supported for now
+; the following symbols are supported for now:
 ;   entity variant defmacro cond
 ;   not and or + - * / mod max min = <> < > <= >=
+; reserved for future use:
+;   import true false
 (defmacro fr/byte-unit [unit]
   (cond
     (= unit "B") "o"
@@ -110,26 +116,3 @@
 <notification-count [(my-plural $n)]
   :one "{{ $n }} notification"
   :many "{{ $n }} notifications">
-
-
-; open questions
-; --------------
-;
-; macros are entities with logic; programmable mappings.  perhaps use the same 
-; syntax as with entities?
-;   <my-plural [x] (cond …)>
-;
-; should we enforce namespaces on custom (private, local) macros?
-; can we do without custom entities? (use macros instead)
-;   or, identifiers starting with -- are private?
-;
-; how to solve accesskeys?
-;  proposal:  each string can be followed by an optional accesskey definition 
-;  prefixed with &
-;    <brand-name "Firefox" &F>
-;  these annotations would be per-string:
-;    <settings [(sys/os)]
-;      :windows "Options" &O
-;      :macos "Preferences" &P
-;      :linux "Settings" &S>
-;
